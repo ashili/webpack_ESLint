5fef789dc4f80611af468553353a53f326cbd3c9
anisshili <anisshili.as@gmail.com>
2021-04-26 23:04:03 -0400

    use const instead of let since there is no plan to chnage the value of the variable

--- src/js/getJSON.js
+++ src/js/getJSON.js
@@ -1,9 +1,8 @@
 export default function getJSON(url, callback) {
-  let xhr;
-  xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
   xhr.responseType = 'json';
-  xhr.onload = function () {
+  xhr.onload = () => {
     const { status } = xhr;
     if (status === 200) {
       callback(null, xhr.response);
